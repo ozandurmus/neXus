8f275bd36a660f42fa27e242d6e26060c23a4dd7
ozandurmus <ozan.durmus@outlook.com.tr>
2026-09-17 10:44:46 +0300

    fix(ui): compact role display with tooltip

--- ui2/frontend/src/shell/TopAppBar.tsx
+++ ui2/frontend/src/shell/TopAppBar.tsx
@@ -1,5 +1,6 @@
 import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
+import Tooltip from "@mui/material/Tooltip";
 import { m3 } from "../theme/m3Theme";
 import { Icon } from "./Icon";
 import { M3Button, StatusChip } from "./M3Widgets";
@@ -63,7 +64,13 @@ export function TopAppBar() {
               <Typography variant="body1" sx={{ fontWeight: 500, color: m3.onSurface }}>
                 {session.displayName}
               </Typography>
-              <Typography variant="body2">{session.roleTokens.join(", ")}</Typography>
+              <Tooltip title={session.roleTokens.join(", ")}>
+                <Typography variant="body2" sx={{ cursor: "default" }}>
+                  {session.roleTokens.length > 2
+                    ? `${session.roleTokens[0]} (+${session.roleTokens.length - 1} roles)`
+                    : session.roleTokens.join(", ")}
+                </Typography>
+              </Tooltip>
             </Box>
             <M3Button emphasis="text" onClick={session.onSignOut}>
               Sign out
